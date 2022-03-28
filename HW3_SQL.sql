0ac6e1b4f9ebc97da540fcfbf4ab301349ef75f8
Maximus2806 <morozof_78@mail.ru>
2022-03-28 12:55:35 +0300

    Update HW3_SQL.sql

--- HW3_SQL.sql
+++ HW3_SQL.sql
@@ -11,4 +11,13 @@ join employee_salary27 on employees27.id=employee_salary27.employee_id
 join salary27 on salary27.id=employee_salary27.salary_id
 where monthly_salary < 2000;
 --3. Show all the rows with salaries where there is no employee assigned to. Вывести все зарплатные позиции, но работник по ним не назначен. (ЗП есть, но не понятно кто её получает.)
-select salary27
+select salary27.monthly_salary as salary
+from salary27
+left join employee_salary27 on salary27.id=employee_salary27.salary_id
+where employee_salary27.salary_id is null;
+--4. Вывести все зарплатные позиции  меньше 2000 но работник по ним не назначен. (ЗП есть, но не понятно кто её получает.)
+select salary27.monthly_salary as salary
+from salary27
+left join employee_salary27 on salary27.id=employee_salary27.salary_id
+where monthly_salary<2000 and employee_salary27.salary_id is null;
+
